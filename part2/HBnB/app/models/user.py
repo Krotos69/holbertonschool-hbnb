4804ba5ffeff52da7ad423ea2a1eb56920f85e20
eugenio martinez <emartinez714@gmail.com>
2026-02-16 21:38:18 -0400

    Task00_app/models/base.py_Business Logic Layer — Models

---
+++ part2/HBnB/app/models/user.py
@@ -0,0 +1,9 @@
+from app.models.base import BaseModel
+
+class User(BaseModel):
+    def __init__(self, email, password, first_name="", last_name=""):
+        super().__init__()
+        self.email = email
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
