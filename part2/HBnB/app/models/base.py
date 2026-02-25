aaf42463575a7c4cbd4b096a60da9e4fdf0b71e7
eugenio martinez <emartinez714@gmail.com>
2026-02-25 15:12:32 -0400

    task01 Implement the classes based_Handle attribute validation and updates

--- part2/HBnB/app/models/base.py
+++ part2/HBnB/app/models/base.py
@@ -7,5 +7,11 @@ class BaseModel:
         self.created_at = datetime.utcnow()
         self.update_at = datetime.utcnow()
 
+def update(self, **kwargs):  # Update attributes of the instance based on provided keyword arguments
+    for key, value in kwargs.items(): # Iterate through the provided keyword arguments and update the corresponding attributes of the instance
+        if hasattr(self, key): # Check if the instance has the attribute specified by the key
+            setattr(self, key, value) # Update the attribute of the instance with the new value
+    self.updated_at = datetime.utcnow() # Update the 'updated_at' attribute to the current UTC time after updating the instance's attributes
+
     def to_dict(self):
         return self.__dict__
