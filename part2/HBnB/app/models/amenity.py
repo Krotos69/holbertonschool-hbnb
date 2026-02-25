45e70226d75789d9f151ec4698e8912e38f708a4
eugenio martinez <emartinez714@gmail.com>
2026-02-25 16:13:31 -0400

    task01 Handle attribute validation and updates name

--- part2/HBnB/app/models/amenity.py
+++ part2/HBnB/app/models/amenity.py
@@ -1,7 +1,11 @@
 from app.models.base import BaseModel
 
-class Amenity(BaseModel):
-    def __init__(self, name, description=""):
+class Amenity(BaseModel): 
+    def __init__(self, name, description=""): # amenity name is required, description is optional
         super().__init__()
-        self.name = name
-        self.description = description
+
+        if not name: # amenity name is required
+            raise ValueError("Amenity name is required") # raise an error if name is not provided
+
+        self.name = name # set the name of the amenity
+        self.description = description # set the description of the amenity, default is an empty string if not provided
