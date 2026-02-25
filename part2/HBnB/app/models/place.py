ae33a73105fb6af1c35329afa4cdfacbde16866b
eugenio martinez <emartinez714@gmail.com>
2026-02-25 15:49:00 -0400

    task01 updates naem owner price required and handle validation

--- part2/HBnB/app/models/place.py
+++ part2/HBnB/app/models/place.py
@@ -3,11 +3,23 @@ from app.models.base import BaseModel
 class Place(BaseModel):
     def __init__(self, name, description="",rooms=0, bathrooms=0, max_guests=0, price_per_night=0.0, address="", owner_id=None):
         super().__init__()
+        
+        if not name: # name is required
+            raise ValueError("place name is required")
+        if owner_id is None: # owner_id is required
+            raise ValueError("owner_id is required")
+        if price_per_night < 0: # price cannot be negative
+            raise ValueError("price cannot be negative")
+
         self.name = name
         self.description = description
         self.rooms = rooms
         self.bathrooms =bathrooms
-        self.mas_guests = mas_guests
+        self.max_guests = max_guests
         self.price_per_night = price_per_night
         self.address = address
         self.owner_id = owner_id
+        
+        # Relationship: place -> reviews
+        self.reviews = [] # This will hold Review instances related to this Place
+        self.amenities = [] # This will hold Amenity instances related to this Place
