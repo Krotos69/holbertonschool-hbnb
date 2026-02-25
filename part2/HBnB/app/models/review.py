c6c4ab5c7fc9fc819333dd9eee09af24f7ab3a6d
eugenio martinez <emartinez714@gmail.com>
2026-02-25 16:05:04 -0400

    task01 rating validation

--- part2/HBnB/app/models/review.py
+++ part2/HBnB/app/models/review.py
@@ -1,9 +1,13 @@
 from app.models.base import BaseModel
 
 class Review(BaseModel):
-    def __init__(self, text, rating, user_id, place_id):
+    def __init__(self, text, rating, user_id, place_id): # Initialize Review with text, rating, user_id, and place_id
         super().__init__()
-        self.text = text
+
+        if rating < 1 or rating > 5: # Ensure rating is between 1 and 5 
+            raise ValueError("Rating must be between 1 and 5") # Validate rating value
+
+        self.text = text # Set the review text
         self.rating = rating
         self.user_id = user_id
         self.place_id = place_id
