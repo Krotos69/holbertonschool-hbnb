5b7896a87e78bd1050bd8b270071f326c0c3de66
eugenio martinez <emartinez714@gmail.com>
2026-02-25 15:24:37 -0400

    task01 Implement email set email password initializa empty list relationship with places and  reviews

--- part2/HBnB/app/models/user.py
+++ part2/HBnB/app/models/user.py
@@ -3,7 +3,17 @@ from app.models.base import BaseModel
 class User(BaseModel):
     def __init__(self, email, password, first_name="", last_name=""):
         super().__init__()
+
+        if not email: # Check if the email is provided, if not, raise a ValueError
+            raise ValueError("Email is required")
+        if not password:
+            raise ValueError("Password is required") # Set the email, password, first name, and last name attributes of the User instance based on the provided arguments
+
         self.email = email
         self.password = password
         self.first_name = first_name
         self.last_name = last_name
+
+        # Relationship: user -> Place, reviews
+        self.places = [] # Initialize an empty list to hold the places associated with the user
+        self.reviews = [] # Initialize an empty list to hold the reviews associated with the user
