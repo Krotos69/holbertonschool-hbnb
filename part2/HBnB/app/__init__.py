50434671b1fee408eb2639492ba5c5e579d69348
eugenio martinez <emartinez714@gmail.com>
2026-03-17 17:20:38 -0400

    task04 2fix typo

--- part2/HBnB/app/__init__.py
+++ part2/HBnB/app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from app.api.vi import users, palces, reviews, amenities
+from app.api.v1 import users, places, reviews, amenities
 
 def create_app():
     app = Flask(__name__)
 
-    #Resgister blueprints
+    # Register blueprints
     app.register_blueprint(users.bp)
     app.register_blueprint(places.bp)
     app.register_blueprint(reviews.bp)
