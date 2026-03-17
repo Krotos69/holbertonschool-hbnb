e0a00dba60317c78ec5f27f02373ef9bf28ab206
eugenio martinez <emartinez714@gmail.com>
2026-03-17 17:14:57 -0400

    task04 fix class name

--- part2/HBnB/app/persistence/repository.py
+++ part2/HBnB/app/persistence/repository.py
@@ -1,17 +1,17 @@
-class InMemoryRespository:
+class InMemoryRepository:
     def __init__(self):
         self.storage = {}
-
+# --- Task 2: Implement the save method to store an object in the repository
     def save(self, obj):
         cls =obj.__class__.__name__
         if cls not in self.storage:
             self.storage[cls] = {}
         self.storage[cls][obj.id] = obj
         return obj
-
-    def get(self, cls, obj_if):
+# --- Task 3: Implement the get method to retrieve an object by its class and ID
+    def get(self, cls, obj_id):
         return self.storage.get(cls.__name__, {}).get(obj_id)
-
+# --- Task 3: Implement the all method to retrieve all objects of a given class
     def all(self, cls):
         return list(self.storage.get(cls.__name__, {}).values())
 
