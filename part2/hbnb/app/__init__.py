a0f77bffa4fc59a6cbeb952e1abac0300d089bdf
eugenio martinez <emartinez714@gmail.com>
2026-07-03 01:06:36 -0400

    update import

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -2,8 +2,7 @@ from flask import Flask
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
-
-
+from app.api.v1.places import api as places_ns
 
 def create_app():
     app = Flask(__name__)
@@ -11,11 +10,11 @@ def create_app():
         app,
         version='1.0',
         title='HBnB API',
-        description='HBnB Application API',
+        description='HBnB REST API',
         doc='/api/v1/'
     )
 
     api.add_namespace(users_ns, path='/api/v1/users')
-    return app
-
+    api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    return app
