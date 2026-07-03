25e0f1f154ac32078ee8e1f117de6e9b4e3d6f65
eugenio martinez <emartinez714@gmail.com>
2026-07-03 00:46:59 -0400

    correted imports and place

--- part2/hbnb/app/api/__init__.py
+++ part2/hbnb/app/api/__init__.py
@@ -1,12 +1,22 @@
+from flask import Flask
 from flask_restx import Api
-from .v1.places import api as places_ns
-from .v1.amenities import api as amenities_ns  # already from previous task
+from app.api.v1.users import api as users_ns
+from app.api.v1.amenities import api as amenities_ns
+from app.api.v1.places import api as places_ns
 
-api = Api(
-    title='HBnB API',
-    version='1.0',
-    description='HBnB REST API'
-)
 
-api.add_namespace(places_ns, path='/api/v1/places')
-api.add_namespace(amenities_ns, path='/api/v1/amenities')
+def create_app():
+    app = Flask(__name__)
+    api = Api(
+        app,
+        version='1.0',
+        title='HBnB API',
+        description='HBnB REST API',
+        doc='/api/v1/'
+    )
+
+    api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(places_ns, path='/api/v1/places')
+    api.add_namespace(amenities_ns, path='/api/v1/amenities')
+
+    return app
