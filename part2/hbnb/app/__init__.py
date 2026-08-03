677fac33d65cc4458c5af8b290160b87f88f42f5
eugenio martinez <emartinez714@gmail.com>
2026-08-03 13:35:35 -0400

    update & fix Blueprint

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -1,20 +1,10 @@
 from flask import Flask
-from flask_restx import Api
-from hbnb.app.api.v1.users import api as users_ns
-from app.api.v1.amenities import api as amenities_ns
-from app.api.v1.places import api as places_ns
+from app.api.v1 import v1 as api_v1
 
 def create_app():
+    """
+    create and configure the place application instance
+    """
     app = Flask(__name__)
-    api = Api(
-        app,
-        version='1.0',
-        title='HBnB API',
-        description='HBnB REST API',
-        doc='/api/v1/'
-    )
-
-    api.add_namespace(users_ns, path='/api/v1/users')
-    api.add_namespace(places_ns, path='/api/v1/places')
-    api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    app.register_blueprint(api_v1)
     return app
