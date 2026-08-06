ec66dd9996b2fc5c68fde2dbe8b04c2dc9d0f643
eugenio martinez <emartinez714@gmail.com>
2026-08-06 17:38:47 -0400

    part3 task2 create auth.py authentication file to create JWT Token

--- part3/hbnb/app/api/v1/auth.py
+++ part3/hbnb/app/api/v1/auth.py
@@ -2,6 +2,7 @@ from flask_restx import Namespace, Resource, fields
 from flask_jwt_extended import create_access_token
 from app.services import facade
 
+# part3: Implement JWT Authentication for User Login
 api = Namespace('auth', description='Authentication operations')
 
 login_model = api.model('Login', {
