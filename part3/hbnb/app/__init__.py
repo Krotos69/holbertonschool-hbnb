aeb95ffa4c05628a45690dffc2900ab81bfc4c4b
eugenio martinez <emartinez714@gmail.com>
2026-08-06 17:20:15 -0400

    add & update JWTManager

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -3,8 +3,10 @@ from flask_bcrypt import Bcrypt
 from app.models import db
 import config 
 from app.api.v1 import v1 as api_v1
+from flask_jwt_extended import JWTManager # task2: import JWTManager from flask_jwt_extended
 
 bcrypt = Bcrypt()
+jwt = JWTManager()
 
 def create_app(config_class=config.DevelopmentConfig):
     """
@@ -17,5 +19,6 @@ def create_app(config_class=config.DevelopmentConfig):
 
     db.init_app(app)  #Initialize the database with the Flask app task1
     bcrypt.init_app(app)    #Register blueprints here task1
+    jwt.init_app(app)       #Initialize JWT manager task2
 
     return app
