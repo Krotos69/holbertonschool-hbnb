8ce330f9d29140fd3519b7852561db49b08ada6f
eugenio martinez <emartinez714@gmail.com>
2026-08-06 15:48:14 -0400

    part3 task1 Register Bcrypt configuration

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
+from app.models import db
+import config 
 from app.api.v1 import v1 as api_v1
 
-def create_app(config_class="config.DevelopmentConfig"):
+bcrypt = Bcrypt()
+
+def create_app(config_class=config.DevelopmentConfig):
     """
     Updated create_app with configuration support, update your factory 
     so it receives a config class (or instance), loads it with from_object, 
@@ -9,7 +14,8 @@ def create_app(config_class="config.DevelopmentConfig"):
     """
     app = Flask(__name__)
     app.config.from_object(config_class)
-    #Initialize extensions here
-    #Register blueprints here
-    
+
+    db.init_app(app)  #Initialize the database with the Flask app task1
+    bcrypt.init_app(app)    #Register blueprints here task1
+
     return app
