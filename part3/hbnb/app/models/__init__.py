5d462c6e053d5d23f00752a24bf87f888de4cc6d
eugenio martinez <emartinez714@gmail.com>
2026-08-08 13:33:15 -0400

    part3 task5  Implement SQLAlchemy Repository update

--- part3/hbnb/app/models/__init__.py
+++ part3/hbnb/app/models/__init__.py
@@ -1,20 +1,5 @@
-"""
-HBnB Models Package
-
-This package contains all the business logic models for the HBnB application.
-It includes the base model class and all entity classes with their relationships.
-"""
-
-from .base_model import BaseModel
+from .storage import storage
 from .user import User
-from .amenity import Amenity
-from .place import Place
-from .review import Review
-
-__all__ = [
-    'BaseModel',
-    'User', 
-    'Amenity',
-    'Place',
-    'Review'
-]
+from .places import Place
+from .amenities import Amenity
+from .reviews import Review
