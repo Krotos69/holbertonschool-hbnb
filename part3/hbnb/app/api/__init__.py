7014d1f7f49b5a1e3bdc79ec3ba93f1cde6232c4
eugenio martinez <emartinez714@gmail.com>
2026-08-08 13:52:47 -0400

    part3 task5  Implement SQLAlchemy Repository update

--- part3/hbnb/app/api/__init__.py
+++ part3/hbnb/app/api/__init__.py
@@ -1,18 +1,8 @@
-#!/usr/bin/python3
+# app.api package
+from flask import Flask
+from app.api.v1 import v1 as api_v1
 
-from flask import Blueprint
-from flask_restx import Api
-
-from app.api.v1.users import api as user_ns
-from app.api.v1.amenities import api as amenity_ns #import the new namespace
-from app.api.v1.places import api as place_ns
-from app.api.v1.reviews import reviews_ns
-
-api_bp = Blueprint('api', __name__, url_prefix='/api/v1')
-api = Api(api_bp, title='HBnB API', version='1.0', description='HBnB API documentation')
-
-# Register namespaces
-api.add_namespace(user_ns, path='/users')
-api.add_namespace(amenity_ns, path='/amenities')  # register amenities namespace
-api.add_namespace(place_ns, path='/places')
-api.add_namespace(reviews_ns, path='/reviews')
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(api_v1)
+    return app
