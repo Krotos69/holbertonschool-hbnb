96842dbea8356c6cd4a54e77ac178705a1c2ddc3
eugenio martinez <emartinez714@gmail.com>
2026-08-08 21:36:16 -0400

    delete import config

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restx import Api
-import config
+
 
 # Import extensions (NOT from app)
 from app.extensions import db, bcrypt, jwt
@@ -9,13 +9,12 @@ from app.extensions import db, bcrypt, jwt
 from app.api.v1 import v1 as api_v1
 
 
-def create_app(config_class=config.DevelopmentConfig):
+def create_app():
     """
     Application Factory for HBnB Part 3.
     Initializes extensions, loads configuration, and registers blueprints.
     """
     app = Flask(__name__)
-    app.config.from_object(config_class)
 
     # Initialize extensions
     db.init_app(app)
