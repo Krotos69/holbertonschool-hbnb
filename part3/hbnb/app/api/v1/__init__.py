d857482eff228a60efa77ea93d2d115675516a53
eugenio martinez <emartinez714@gmail.com>
2026-08-08 22:29:37 -0400

    part3 task7 fix test

--- part3/hbnb/app/api/v1/__init__.py
+++ part3/hbnb/app/api/v1/__init__.py
@@ -1,25 +1,17 @@
 from flask import Blueprint
 from flask_restx import Api
 
-# Import the namespaces from the packages 
-from app.api.v1.users import api as users_ns
-from app.api.v1.places import api as places_ns
-from app.api.v1.amenities import api as amenities_ns
-from app.api.v1.reviews import api as reviews_ns
+api_v1 = Blueprint('api_v1', __name__, url_prefix='/api/v1')
+api = Api(api_v1, version='1.0', title='HBnB API', description='HBnB REST API')
 
-# Create Blueprint for version 1 of the Api
-v1 = Blueprint('api_v1', __name__, url_prefix='/api/v1')
+# Import namespaces
+from .users import api as users_ns
+from .places import api as places_ns
+from .reviews import api as reviews_ns
+from .amenities import api as amenities_ns
 
-# Create API instance and register the namespaces(objects)
-api = Api(
-	v1,
-    version='1.0',
-    title='HBnB API v1',
-    description='API for HBnB project (Part2)'
-)
-
-# Register the namespaces with the API isntance (objects)
+# Register namespaces
 api.add_namespace(users_ns)
 api.add_namespace(places_ns)
-api.add_namespace(amenities_ns)
 api.add_namespace(reviews_ns)
+api.add_namespace(amenities_ns)
