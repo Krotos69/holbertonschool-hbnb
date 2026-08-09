1cebab266a73d22569989b321e7e10175333199d
eugenio martinez <emartinez714@gmail.com>
2026-08-08 22:32:21 -0400

    this file intentionally left empty

--- part3/hbnb/app/api/__init__.py
+++ part3/hbnb/app/api/__init__.py
@@ -1,8 +1,2 @@
-# app.api package
-from flask import Flask
-from app.api.v1 import v1 as api_v1
-
-def create_app():
-    app = Flask(__name__)
-    app.register_blueprint(api_v1)
-    return app
+# This file intentionally left simple.
+# It only marks "app/api" as a package.
